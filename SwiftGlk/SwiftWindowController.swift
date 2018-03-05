990c1956c988c660d91119f9e9ee27240941f6ef
C.W. Betts <computers57@hotmail.com>
2018-03-05 14:48:03 -0700

    Begin work on simple Swift app. Minor Xcode clean-up.

---
+++ SwiftGlk/SwiftWindowController.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftWindowController.swift
+//  SwiftGlk
+//
+//  Created by C.W. Betts on 3/5/18.
+//
+
+import Cocoa
+import GlkView.GlkView
+
+class SwiftWindowController: NSWindowController, GlkViewDelegate {
+	
+}
